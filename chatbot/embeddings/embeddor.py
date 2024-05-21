a7eb99ec6b2ba58a8c4cb5b5d4b2d6923d3a94c0
Himesh Parashar <himeshparashar424@gmail.com>
2024-05-21 16:35:38 +0530

    refactor: Update embedding and llm modules
    - Update `embedding` module to include the `create_embeddings` method for generating embeddings using the OpenAI API.
    - Update `llm` module to include the `get_llm` function for initializing the ChatOpenAI model with custom parameters.

--- chatbot/embeddings/embeddor.py
+++ chatbot/embeddings/embeddor.py
@@ -1,13 +1,28 @@
 from langchain_chroma import Chroma
-
+import openai
+from fastapi import HTTPException
 
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
+embedding_model = os.getenv("EMBEDDING_MODEL")
+
 os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
 class embedding:
+
+    def create_embeddings(texts):
+        try:
+            res = openai.embeddings.create(input=texts, model=embedding_model)
+            for record in res.data:
+                embeds = record.embedding
+            return embeds
+        except Exception as e:
+            print(e)
+            error_message = str(e)
+            raise HTTPException(status_code=400, detail=error_message)
 
     def embedder(self, splits: str):
         vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
--- chatbot/llm/llm.py
+++ chatbot/llm/llm.py
@@ -1,3 +1,11 @@
 from langchain_openai import ChatOpenAI
+import os
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+def get_llm(model:str='gpt-3.5-turbo')->ChatOpenAI:
+    llm = ChatOpenAI(
+        model=model, 
+        api_key=os.environ["OPENAI_API_KEY"],
+        max_tokens=3000,
+        temperature=0.7
+    )
+    return llm
